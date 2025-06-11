f13ee41703574e515473ee4561b74f63850aed81
casltd\iona <iona@casltd.com>
2025-06-11 13:40:10 +0100

    Getting user input from the console to give to the plant as its name.

--- JenkinsTestApp2/JenkinsTestApp2/Program.cs
+++ JenkinsTestApp2/JenkinsTestApp2/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Testing plant simulator!!!");
-Console.WriteLine(" _,-._\r\n/ \\_/ \\\r\n>-(_)-<       \r\n\\_/ \\_/\r\n  `-'");
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+class Program {
+
+    static void Main(string[] args)
+    {
+        string plantName = "testName";
+
+        //check jenkins args
+        if (args.Length > 0 && !string.IsNullOrEmpty(args[0])) {
+            plantName = args[0];
+        }
+
+        Console.WriteLine("Testing plant simulator!!!");
+        Console.WriteLine(" _,-._\r\n/ \\_/ \\\r\n>-(_)-<       \r\n\\_/ \\_/\r\n  `-'");
+
+        Console.WriteLine("It appears you are now the proud parent of a small little seed, what do you wish to name it?");
+
+        Console.WriteLine($"Ahh... You have named it {plantName}! So sweet of you.");
+    }
+}
