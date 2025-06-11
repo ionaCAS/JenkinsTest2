fbbc012ecaf564ea93ad8245308ce3261e71589b
casltd\iona <iona@casltd.com>
2025-06-11 14:11:44 +0100

    Added plant image and a growth simulation.

--- JenkinsTestApp2/JenkinsTestApp2/Program.cs
+++ JenkinsTestApp2/JenkinsTestApp2/Program.cs
@@ -19,5 +19,18 @@ class Program {
         Console.WriteLine("It appears you are now the proud parent of a small little seed, what do you wish to name it?");
 
         Console.WriteLine($"Ahh... You have named it {plantName}! So sweet of you.");
+
+        Console.WriteLine("                                      ----------         \r\n        -------------            ------_       -----     \r\n     ----            _        ----                 ---_  \r\n   --                  _     --__   --------         --_ \r\n   --                  -------- _----_    -------     ---\r\n  --                 --┐┐_   ┌┌---╞           _--_--_----\r\n --            -------╞ |    |                           \r\n--_    -- --__ - -     ||    |                           \r\n_--_--_-               ||   |X                           \r\n _                     ||   |X                           \r\n                        |   |                            \r\n                        |-  ---                          \r\n                       --    --                          \r\n                      ---    ---                         \r\n                XXXXX -       --X      XXX               \r\n           X X X    XX  XX  X  XXXXX        XX X         \r\n                XXXXXXX X            XXXXXXX             ");
+
+        Console.WriteLine($"It appears now that {plantName} is starting to grow!!!");
+
+        //Growing simulation
+        int growTime = 10; //seconds
+        for (int i = 1; i <= growTime; i++) {
+            Console.WriteLine($"Your plant is growing to... {i} cm tall!");
+            Thread.Sleep(1000);
+        }
+
+        //add input for how long the plant simulation is to last.
     }
 }
